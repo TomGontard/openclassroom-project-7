3c81b0490776fd658c6d9cf80662597df1164a4b
Tom Gontard <tomgontard@protonmail.com>
2025-03-17 14:25:40 +0100

    Mise en place du Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,69 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.svg'
+import styled from 'styled-components'
+import { colors } from '../../utils/style/colors'
+import { fonts } from '../../utils/style/fonts'
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 60px;
+    color: ${colors.primary};
+    font-family: ${fonts.text};
+`
+
+const HomeLogo = styled.img`
+    width: 210px;
+    height: auto;
+
+    @media only screen and (max-width: 600px) {
+        width: 145px;
+    }
+`
+
+const HeaderNav = styled.nav`
+    ul {
+        display: flex;
+        list-style: none;
+    }
+    ul > * {
+        margin-left: 50px;
+    }
+    ul > *:first-child {
+        margin-left: 0;
+    }
+`
+
+const HeaderLink = styled(Link)`
+    text-decoration: none;
+    color: ${colors.primary};
+    font-size: 24px;
+    font-weight: 500;
+    
+    &:hover {
+        text-decoration: underline;
+    }
+
+    @media only screen and (max-width: 600px) {
+        font-size: 16px;
+    }
+`
+
 
 export default function Header() {
     return (
-        <div>
+        <HeaderWrapper>
             <Link to="/">
-                <img src={logo} alt="logo" />
+                <HomeLogo src={logo} alt="logo" />
             </Link>
-            <nav>
+            <HeaderNav>
                 <ul>
-                    <Link to="/"> Accueil </Link>
-                    <Link to="/A-Propos"> A Propos </Link>
+                    <HeaderLink to="/"> Accueil </HeaderLink>
+                    <HeaderLink to="/about"> A Propos </HeaderLink>
                 </ul>
-            </nav>
-        </div>
+            </HeaderNav>
+        </HeaderWrapper>
   )
 }

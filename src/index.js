0086c371dd612b2a368a18b49692da469dcaa9d1
Tom Gontard <tomgontard@protonmail.com>
2025-03-17 13:37:02 +0100

    Ajout de la logique de routes et création des pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Apartment from './pages/Apartment'
+import About from './pages/About'
+import Error from './pages/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Router>
-  </Router>
+    <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/apartment/:apartmentID" element={<Apartment />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+    </Router>
   </React.StrictMode>
 );

36ddb243f9f345f1c8f0695a5934b81d47ad66ba
Tom Gontard <tomgontard@protonmail.com>
2025-03-17 14:42:21 +0100

    Ajout CSS footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,49 @@
+import styled from "styled-components"
 import { Link } from "react-router-dom"
-import Logo from "../../assets/images/logo.svg"
+import { fonts } from "../../utils/style/fonts"
+import { ReactComponent as Logo } from "../../assets/images/logo.svg"
+
+const FooterWrapper = styled.footer`
+    background-color: #000;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 25px;
+    margin-top: 20px;
+    height: 200px;
+    color: white;
+    font-family: ${fonts.text};
+`
+const FooterImage = styled(Logo)`
+    width: 120px;
+    height: auto;
+    margin: 20px 0px 20px 0px;
+
+    path {
+        fill: white;
+    }
+`
+
+const FooterParagraph = styled.p`
+    font-size: 24px;
+    margin: 20px 0px 10px 0px;
+
+    @media only screen and (max-width: 600px) {
+        font-size: 12px;
+    }
+`
 
 export default function Footer() {
     return (
-        <footer>
+        <FooterWrapper>
             <Link to="/">
-                <img src={Logo} alt="Kasa Logo" />
+                <FooterImage src={Logo} alt="Kasa Logo" />
             </Link>
-            <p>
+            <FooterParagraph>
                 &copy; 2020 Kasa. All rights reserved
-            </p>
-        </footer>
+            </FooterParagraph>
+        </FooterWrapper>
     );
 }
